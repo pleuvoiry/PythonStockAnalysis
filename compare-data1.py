67d313499e73119b844238a81bf3b45532589e4a
pleuvoiry <43768622+pleuvoiry@users.noreply.github.com>
2022-05-09 18:31:44 +0900

    update

--- compare-data1.py
+++ compare-data1.py
@@ -1,18 +1,29 @@
 from pandas_datareader import data as pdr
 import yfinance as yf
 import matplotlib.pyplot as plt
+import pandas as pd
 yf.pdr_override()
-
-dow = pdr.get_data_yahoo('^DJI', '2000-01-04')
-kospi = pdr.get_data_yahoo('^KS11', '2000-01-04')  # indexing by "* 100" 지수화
-
-d = (dow.Close / dow.Close.loc['2000-01-04']) * 100
-k = (kospi.Close / kospi.Close.loc['2000-01-04']) * 100
+from Investar import Analyzer
 
 
-plt.figure(figsize=(9, 5))
-plt.plot(d.index, d, 'r--', label='Dow Jones Industrial Average')
-plt.plot(k.index, k, 'b', label='KOSPI')
-plt.grid(True)
+naverURL = ''
+krx_list = pd.read_html('d:/My Project/stockAnalysis/PythonStockAnalysis/상장법인목록_220508.xls')
+
+krx_list[0].종목코드 = krx_list[0].종목코드.map('{:06d}'.format)
+
+#print(krx_list[0].종목코드)
+
+mk = Analyzer.MarketDB()
+df = mk.get_daily_price('005930', '2017-07-10', '2018-06.30')
+
+
+plt.figure(figsize=(9, 6))
+plt.suplot(2, 1, 1)
+plt.title('Samsung Electronics (Investar Data')
+
+plt.plot(df.index, df['close'], 'c', label='Close')
+plt.legend(loc='best')
+plt.subplot(2, 1, 2)
+plt.bar(df.index, df['volume'], color='g', label='Volume')
 plt.legend(loc='best')
 plt.show()
